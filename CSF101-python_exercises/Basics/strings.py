107e31c697eaa534742740ef9a22a4464e67a7d7
kinlay123 <201.00308.11.0749@education.gov.bt>
2024-09-01 20:23:11 -0700

    Add files via upload

---
+++ CSF101-python_exercises/Basics/strings.py
@@ -0,0 +1,8 @@
+name="kinlay Gyeltshen"
+print(name)
+greeting="Hello," + name + "!"
+print(greeting)
+greeting_f=f"hello,{name}!"
+print(greeting_f)
+name_length = len(name)
+print(name_length)
---
+++ CSF101-python_exercises/Basics/type-conversion.py
@@ -0,0 +1,5 @@
+age = 1
+age_str = str(age)
+message = "i am" + age_str + "year old"
+print= (message)
+
